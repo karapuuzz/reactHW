b91e73d0bd8174aff3aa6abedec90a0edfb318de
karapuuzz <koloskovilla0@gmail.com>
2022-09-14 17:37:44 +0300

    lesson1 homework

---
+++ src/components/simpsons.js
@@ -0,0 +1,15 @@
+// import {simpsons} from "./simpsonsdata";
+//
+// function simpsonsfamily() {
+//     return(<div>
+//             {simpsons.map(simpson => (<div>
+//                 <h2>{simpson.id}  {simpson.name}</h2>
+//                 <p>{simpson.description}</p>
+//             </div>))}
+//         </div>
+//
+//     )
+//
+// }
+//
+// export default simpsonsfamily;
