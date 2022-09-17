e15c8796388f697d81dd7dc6649b1d36f4b78736
karapuuzz <koloskovilla0@gmail.com>
2022-09-17 23:09:57 +0300

    lesson1 homework

--- src/App.js
+++ src/App.js
@@ -1,40 +1,25 @@
-// import logo from './logo.svg';
+import logo from './logo.svg';
 import './App.css';
-import {simpsons} from "./components/simpsonsdata";
 
 function App() {
-  return(<div>
-        {simpsons.map(simpson => (<div>
-          <h2>{simpson.id}  {simpson.name}</h2>
-          <p>{simpson.description}</p>
-        </div>))}
-      </div>
-
-  )
-
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
-//
-// export default simpsonsfamily;
-
-// functionction App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
